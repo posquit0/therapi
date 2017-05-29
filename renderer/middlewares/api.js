8b3e37eb1c124212ce0e3d9b06b881aade2b82cc
Posquit0.BJ <posquit0.bj@gmail.com>
2017-05-29 19:49:16 +0900

    Update api middleware

--- renderer/middlewares/api.js
+++ renderer/middlewares/api.js
@@ -1,34 +1,51 @@
 import axios, { CancelToken } from 'axios';
+import uuidV4 from 'uuid/v4';
 import { SEND_REQUEST, CANCEL_REQUEST } from '../actions/request';
 import { setResponse } from '../actions/response';
+import { addHistory } from '../actions/history';
 
 
-const canceled = new Map();
+// TODO: as user preference
+const agent = axios.create({
+  timeout: 10000,
+  headers: {
+    'User-Agent': 'Therapi/1.0.0'
+  },
+  validateStatus: status => status >= 200 && status < 600
+});
+
+const pending = new Map();
 
 const apiMiddleware = ({ getState, dispatch }) => next => action => {
-  const requestId = '4444';
-  // const requestId = action.payload.request.id;
-  const handleResponse = (response) => {
+  // TODO: refactor
+  const handleResponse = (id, response) => {
+    const item = pending.get(id);
+    pending.delete(id);
+    response.latency = new Date() - item.createdAt;
+    item.response = response;
     dispatch(setResponse(response));
+    dispatch(addHistory(item));
   };
   const handleError = (error) => {
-    if (requestId && canceled.get(requestId))
-      return;
+    console.log(error);
+    return;
   };
 
   switch (action.type) {
-    case SEND_REQUEST:
-      const { request } = getState().local;
-      axios(request)
-        .then(handleResponse)
-        .catch(handleError);
+  case SEND_REQUEST:
+    const id = uuidV4();
+    const { request } = getState().local;
+    const createdAt = new Date();
+    pending.set(id, { id, request, createdAt });
+    agent(request)
+      .then(response => handleResponse(id, response))
+      .catch(handleError);
     return;
   case CANCEL_REQUEST:
-      canceled.set(requestId, true);
-      setTimeout(() => canceled.delete(requestId), 5000);
+    return;
+  default:
+    return next(action);
   }
-
-  return next(action);
 };
 
 export default apiMiddleware;
